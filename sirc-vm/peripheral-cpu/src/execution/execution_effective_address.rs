fd9a521c2ccac4044baef9e62add1dc3ffb0e197
Sean Dawson <contact@seandawson.info>
2023-07-24 20:39:27 +1000

    refactor: possibly make branching a bit more readable

--- sirc-vm/peripheral-cpu/src/execution/execution_effective_address.rs
+++ sirc-vm/peripheral-cpu/src/execution/execution_effective_address.rs
@@ -61,18 +61,28 @@ impl StageExecutor for ExecutionEffectiveAddressExecutor {
 
             ExecutionStepInstructionType::MemoryRefDisplacement => {
                 let (displaced, _) = decoded.ad_l_.overflowing_add(decoded.sr_b_);
-                intermediate_registers.alu_output = displaced;
+                // This is the original address with the pre/post increment/decrement applied (no offset)
+                // we need this value to write back to the source address register to do the inc/dec
+                let (incremented_src, _) = decoded.ad_l_.overflowing_add(decoded.addr_inc as u16);
 
-                if decoded.addr_inc == 0 {
-                    intermediate_registers.address_output = displaced;
-                } else {
-                    (intermediate_registers.address_output, _) =
-                        decoded.ad_l_.overflowing_add(decoded.addr_inc as u16);
-
-                    if decoded.addr_inc < 0 {
+                match decoded.addr_inc {
+                    -1 => {
+                        // alu_output drives the memory store so it needs to PRE decremented here
                         (intermediate_registers.alu_output, _) =
                             displaced.overflowing_add(decoded.addr_inc as u16);
+                        intermediate_registers.address_output = incremented_src;
                     }
+                    0 => {
+                        intermediate_registers.alu_output = displaced;
+                        // If we aren't doing a pre decrement/post increment,
+                        // the address we want to write is just the calculated address
+                        intermediate_registers.address_output = displaced;
+                    }
+                    1 => {
+                        intermediate_registers.alu_output = displaced;
+                        intermediate_registers.address_output = incremented_src;
+                    }
+                    _ => panic!("addr_inc should never not be -1, 0 or 1"),
                 }
             }
 
