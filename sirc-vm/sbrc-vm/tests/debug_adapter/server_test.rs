d56a6f89ff5b763d8f60ed44c0cfeb8dc27b7ca8
Sean Dawson <contact@seandawson.info>
2024-06-14 14:47:24 +1000

    build: try collecting coverage from examples

--- sirc-vm/sbrc-vm/tests/debug_adapter/server_test.rs
+++ sirc-vm/sbrc-vm/tests/debug_adapter/server_test.rs
@@ -1,5 +1,8 @@
 use sbrc_vm::debug_adapter::server::{format_instruction_ref, parse_instruction_ref};
 
+// TODO: Make these unit tests
+// https://doc.rust-lang.org/book/ch11-03-test-organization.html#testing-private-functions
+
 #[test]
 fn test_format_instruction_ref() {
     assert_eq!("pc:ABC", format_instruction_ref(0xABC));
