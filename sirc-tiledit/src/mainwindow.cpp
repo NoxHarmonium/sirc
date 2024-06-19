5f284b852e42aad4ddd846f4a09d17c708832694
Sean Dawson <contact@seandawson.info>
2024-06-19 19:32:38 +1000

    chore: boilerplate for qt6 application

---
+++ sirc-tiledit/src/mainwindow.cpp
@@ -0,0 +1,9 @@
+#include "./ui_mainwindow.h"
+#include <mainwindow.h>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() { delete ui; }
