93dc5964e2d447a3ca298227d911d41fac4667f7
Sean Dawson <contact@seandawson.info>
2025-07-13 15:43:03 +1000

    build: generate header for toolchain-interop crate
    - I'm checking it in for now to make it easier to get things up and running on a fresh checkout
    - The C++ project will compile out of the box once it it includes the header file, but not link until the rust project is built

---
+++ sirc-vm/toolchain-interop/include/sirc_toolchain_interop.h
@@ -0,0 +1,32 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+struct CTilemap {
+  const uint16_t *packed_pixel_data;
+  uintptr_t packed_pixel_data_len;
+  uint16_t palette[16];
+};
+
+extern "C" {
+
+/// Takes tilemap data and returns a string that contains assembly code that will assemble to the
+/// same tilemap.
+///
+/// Useful when you want to embed tilemap data in a program, which is probably the simplest way to
+/// do it at the moment (although it might be possible to just link in a raw binary file).
+char *tilemap_to_str(CTilemap tilemap);
+
+/// Frees a string returned by one of the rust functions in this file.
+///
+/// Should be called when the string is no longer needed to avoid memory leaks.
+///
+/// # Safety
+///
+/// The parameter passed to this function _must_ have been allocated by one of the rust functions
+/// in this file.
+void free_str(char *str);
+
+}  // extern "C"
