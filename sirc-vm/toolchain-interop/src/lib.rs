dadbb637027b7671d965c8df07a3d0344e8f12d2
Sean Dawson <contact@seandawson.info>
2025-07-13 15:18:55 +1000

    feat: add c compatible function to print tilesets

---
+++ sirc-vm/toolchain-interop/src/lib.rs
@@ -0,0 +1,66 @@
+/// This crate isolates FFI functions from the rest of the workspace to keep everything tidy
+use std::ffi::c_char;
+use std::slice;
+use toolchain::printers::shared::print_tokens;
+use toolchain::types::data::{DataToken, DataType};
+use toolchain::types::shared::{NumberToken, NumberType, Token};
+
+#[repr(C)]
+pub struct CTilemap {
+    pub packed_pixel_data: *const u16,
+    pub packed_pixel_data_len: usize,
+    pub palette: [u16; 16],
+}
+
+fn u16_to_data_token(x: u16) -> Token {
+    Token::Data(DataToken {
+        size_bytes: 2,
+        value: DataType::Value(NumberToken {
+            value: u32::from(x),
+            number_type: NumberType::Hex,
+        }),
+    })
+}
+
+fn slice_to_data_tokens(x: &[u16]) -> Vec<Token> {
+    x.iter()
+        .map(|x| u16_to_data_token(*x))
+        .collect::<Vec<Token>>()
+}
+
+/// Takes tilemap data and returns a string that contains assembly code that will assemble to the
+/// same tilemap.
+///
+/// Useful when you want to embed tilemap data in a program, which is probably the simplest way to
+/// do it at the moment (although it might be possible to just link in a raw binary file).
+#[unsafe(no_mangle)]
+pub extern "C" fn tilemap_to_str(tilemap: CTilemap) -> *mut c_char {
+    let packed_pixel_data = if tilemap.packed_pixel_data.is_null() {
+        &[]
+    } else {
+        unsafe { slice::from_raw_parts(tilemap.packed_pixel_data, tilemap.packed_pixel_data_len) }
+    };
+    let pixel_tokens = slice_to_data_tokens(packed_pixel_data);
+    let palette_tokens = slice_to_data_tokens(&tilemap.palette);
+    let all_tokens = [pixel_tokens, palette_tokens].concat();
+    let output_string = print_tokens(&all_tokens);
+
+    let c_str = std::ffi::CString::new(output_string).unwrap();
+    c_str.into_raw()
+}
+
+/// Frees a string returned by one of the rust functions in this file.
+///
+/// Should be called when the string is no longer needed to avoid memory leaks.
+///
+/// # Safety
+///
+/// The parameter passed to this function _must_ have been allocated by one of the rust functions
+/// in this file.
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn free_str(str: *mut c_char) {
+    unsafe {
+        let c_str = std::ffi::CString::from_raw(str);
+        drop(c_str);
+    }
+}
--- sirc-vm/toolchain/src/types/instruction.rs
+++ sirc-vm/toolchain/src/types/instruction.rs
@@ -1,7 +1,7 @@
 use crate::types::data::RefToken;
 use peripheral_cpu::coprocessors::processing_unit::definitions::InstructionData;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct InstructionToken {
     /// The length of the parser input at the time of parsing, used to work out where the parser is in the file
     pub input_length: usize,
