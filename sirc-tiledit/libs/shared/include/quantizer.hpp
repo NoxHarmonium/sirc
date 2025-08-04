18ce99dc39cdb56bd7dcce1df525225b4d02f48d
Sean Dawson <contact@seandawson.info>
2025-08-05 07:42:13 +1000

    feat: add method to quantize multiple images at once
    - That means we can opimise tilesets by allowing them to share a palette

--- sirc-tiledit/libs/shared/include/quantizer.hpp
+++ sirc-tiledit/libs/shared/include/quantizer.hpp
@@ -13,8 +13,30 @@ class Quantizer {
 public:
   Quantizer() = default;
 
+  /**
+   * Takes an indexed image with a palette and reduces the palette so it can be
+   * referenced by pixel values with the given bpp.
+   * E.g. 4bpp = 16 colours addressable -> therefore, max 16 colours
+   * @param sircImage the image to quantise the palette for
+   * @param bpp determines the max size of the palette
+   * @return a copy of the image with the palette remapped
+   */
   [[nodiscard]] virtual SircImage quantize(const SircImage &sircImage,
                                            PaletteReductionBpp bpp) const = 0;
+  /**
+   * Takes indexed images with palettes, combines their palettes and reduces the
+   * combined palette so it can be referenced by pixel values with the given
+   * bpp. E.g. 4bpp = 16 colours addressable -> therefore, max 16 colours
+   *
+   * Useful when you want different tilemaps to share the same palette.
+   *
+   * @param sircImages the images to quantise the palette for
+   * @param bpp determines the max size of the palette
+   * @return a copy of the image with the palette remapped
+   */
+  [[nodiscard]] virtual std::vector<SircImage>
+  quantize_all(const std::vector<SircImage> &sircImages,
+               PaletteReductionBpp bpp) const = 0;
 
   Quantizer(const Quantizer &) = default;
   Quantizer &operator=(const Quantizer &) = default;
--- sirc-tiledit/libs/shared/tests/mediancutquantizer.test.cpp
+++ sirc-tiledit/libs/shared/tests/mediancutquantizer.test.cpp
@@ -6,7 +6,7 @@
 #include <mediancutquantizer.hpp>
 #include <sircimage.hpp>
 
-TEST_CASE("Reduces palette size to 2bpp", "[quantize]") {
+TEST_CASE("Single Image - Reduces palette size to 2bpp", "[quantize]") {
   const SircImage sircImage = {.palette = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
                                .pixelData = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
 
@@ -24,4 +24,30 @@ TEST_CASE("Reduces palette size to 2bpp", "[quantize]") {
                       [&palette](const PaletteReference pixel) {
                         return pixel < palette.size();
                       }));
+}
+
+TEST_CASE("Multiple Images - Reduces palette size to 2bpp", "[quantize]") {
+  const SircImage sircImage1 = {
+      .palette = {7, 8, 9, 10, 11, 7, 8, 9, 10, 11},
+      .pixelData = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
+  const SircImage sircImage2 = {
+      .palette = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6},
+      .pixelData = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
+
+  const auto quantizer = MedianCutQuantizer();
+  const auto quantizedImages = quantizer.quantize_all(
+      {sircImage1, sircImage2}, PaletteReductionBpp::TwoBpp);
+
+  for (const auto &quantizedImage : quantizedImages) {
+    const auto [palette, pixelData] = quantizedImage;
+    const std::vector<SircColor> expectedPalette = {1, 4, 7, 10};
+
+    REQUIRE(4 == palette.size());
+    REQUIRE(std::is_permutation(palette.cbegin(), palette.cend(),
+                                expectedPalette.cbegin()));
+    REQUIRE(std::all_of(pixelData.cbegin(), pixelData.cend(),
+                        [&palette](const PaletteReference pixel) {
+                          return pixel < palette.size();
+                        }));
+  }
 }
