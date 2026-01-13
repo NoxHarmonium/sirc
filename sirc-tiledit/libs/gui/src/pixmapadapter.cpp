ac34591a07d45b1b5b8f686437d448c278a1ee0a
Sean Dawson <contact@seandawson.info>
2026-01-13 16:03:55 +1100

    fix: bug where tilemaps would be rotated

--- sirc-tiledit/libs/gui/src/pixmapadapter.cpp
+++ sirc-tiledit/libs/gui/src/pixmapadapter.cpp
@@ -54,7 +54,7 @@ QPixmap PixmapAdapter::sircImageToPixmap(const SircImage &sircImage) {
   for (int x = 0; x < WIDTH_PIXELS; x++) {
     for (int y = 0; y < HEIGHT_PIXELS; y++) {
       // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
-      const auto paletteColor = pixelData[(x * WIDTH_PIXELS) + y];
+      const auto paletteColor = pixelData[y * WIDTH_PIXELS + x];
       assert(paletteColor <= palette->size());
       const auto sircColor = palette->empty() ? 0 : palette->at(paletteColor);
 
--- sirc-tiledit/libs/shared/src/rgbaadapter.cpp
+++ sirc-tiledit/libs/shared/src/rgbaadapter.cpp
@@ -65,7 +65,7 @@ RgbaPixelData RgbaAdapter::sircImageToRgba(const SircImage &sircImage) {
   for (int x = 0; x < WIDTH_PIXELS; x++) {
     for (int y = 0; y < HEIGHT_PIXELS; y++) {
       // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
-      const auto paletteColor = pixelData[x * WIDTH_PIXELS + y];
+      const auto paletteColor = pixelData[y * WIDTH_PIXELS + x];
       assert(paletteColor < palette->size());
       const auto sircColor = palette->at(paletteColor);
       // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-constant-array-index)
